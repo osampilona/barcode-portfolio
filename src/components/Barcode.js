340bb14269deb0d09ae79534176c427b6ca7b2d0
osampilona <brzicamartina@gmail.com>
2021-11-09 19:40:29 +0100

    All links connected and line color fixed

--- src/components/Barcode.js
+++ src/components/Barcode.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import { useTheme } from "../hooks/useTheme";
 import "../styles/Barcode.css";
+import { Link } from "react-router-dom";
 
 function Barcode() {
   const [line, setLine] = useState("not-visible");
   const { mode, antimode } = useTheme();
 
   const lineShow = () => {
-    if (useTheme === "barcode") {
+    if (mode === "light") {
       setLine("visible");
     } else {
       setLine("visible-dark");
     }
 
-    if (useTheme === "barcode dark") {
+    if (mode === "dark") {
       setLine("visible-dark");
     } else {
       setLine("visible");
@@ -28,117 +29,121 @@ function Barcode() {
   };
 
   return (
-    <div
-      className={"barcode " + mode}
-      onMouseEnter={lineShow}
-      onMouseLeave={reset}
-    >
-      <div className={line}></div>
-      <p>---- ABOUT MARTINA ----</p>
-      <div className="all-lines">
-        <section className="long">
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thick ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-        </section>
-        <div className="lines-and-numbers">
-          <section className="short">
-            <div className={`short-thick ${antimode}`}></div>
-            <div className={`short-thin ${antimode}`}></div>
-            <div className={`short-thick ${antimode}`}></div>
-            <div className={`short-thin ${antimode}`}></div>
-            <div className={`short-thin ${antimode}`}></div>
-            <div className={`short-thick ${antimode}`}></div>
-          </section>
-          <div>----</div>
+    <>
+      <Link to="/about" className="link">
+        <div
+          className={"barcode " + mode}
+          onMouseEnter={lineShow}
+          onMouseLeave={reset}
+        >
+          <div className={line}></div>
+          <p>---- ABOUT MARTINA ----</p>
+          <div className="all-lines">
+            <section className="long">
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thick ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+            </section>
+            <div className="lines-and-numbers">
+              <section className="short">
+                <div className={`short-thick ${antimode}`}></div>
+                <div className={`short-thin ${antimode}`}></div>
+                <div className={`short-thick ${antimode}`}></div>
+                <div className={`short-thin ${antimode}`}></div>
+                <div className={`short-thin ${antimode}`}></div>
+                <div className={`short-thick ${antimode}`}></div>
+              </section>
+              <div>----</div>
+            </div>
+            <section className="long">
+              <div className={`long-thick ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thick ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+            </section>
+            <div className="lines-and-numbers">
+              <section className="short">
+                <div className={`short-thick ${antimode}`}></div>
+                <div className={`short-thick ${antimode}`}></div>
+                <div className={`short-thin ${antimode}`}></div>
+                <div className={`short-thin ${antimode}`}></div>
+                <div className={`short-thick ${antimode}`}></div>
+                <div className={`short-thin ${antimode}`}></div>
+              </section>
+              <div>-08-</div>
+            </div>
+            <section className="long">
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thick ${antimode}`}></div>
+              <div className={`long-thick ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+            </section>
+            <div className="lines-and-numbers">
+              <section className="short">
+                <div className={`short-thin ${antimode}`}></div>
+                <div className={`short-thick ${antimode}`}></div>
+                <div className={`short-thick ${antimode}`}></div>
+                <div className={`short-thin ${antimode}`}></div>
+                <div className={`short-thin ${antimode}`}></div>
+                <div className={`short-thick ${antimode}`}></div>
+              </section>
+              <div>-01-</div>
+            </div>
+            <section className="long">
+              <div className={`long-thick ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+            </section>
+            <div className="lines-and-numbers">
+              <section className="short">
+                <div className={`short-thick ${antimode}`}></div>
+                <div className={`short-thin ${antimode}`}></div>
+                <div className={`short-thin ${antimode}`}></div>
+                <div className={`short-thick ${antimode}`}></div>
+                <div className={`short-thick ${antimode}`}></div>
+                <div className={`short-thin ${antimode}`}></div>
+              </section>
+              <div>-1992-</div>
+            </div>
+            <section className="long">
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thick ${antimode}`}></div>
+              <div className={`long-thick ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+            </section>
+            <div className="lines-and-numbers">
+              <section className="short">
+                <div className={`short-thin ${antimode}`}></div>
+                <div className={`short-thin ${antimode}`}></div>
+                <div className={`short-thick ${antimode}`}></div>
+                <div className={`short-thick ${antimode}`}></div>
+                <div className={`short-thin ${antimode}`}></div>
+                <div className={`short-thick ${antimode}`}></div>
+              </section>
+              <div>----</div>
+            </div>
+            <section className="long">
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thick ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+              <div className={`long-thin ${antimode}`}></div>
+            </section>
+          </div>
         </div>
-        <section className="long">
-          <div className={`long-thick ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thick ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-        </section>
-        <div className="lines-and-numbers">
-          <section className="short">
-            <div className={`short-thick ${antimode}`}></div>
-            <div className={`short-thick ${antimode}`}></div>
-            <div className={`short-thin ${antimode}`}></div>
-            <div className={`short-thin ${antimode}`}></div>
-            <div className={`short-thick ${antimode}`}></div>
-            <div className={`short-thin ${antimode}`}></div>
-          </section>
-          <div>-08-</div>
-        </div>
-        <section className="long">
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thick ${antimode}`}></div>
-          <div className={`long-thick ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-        </section>
-        <div className="lines-and-numbers">
-          <section className="short">
-            <div className={`short-thin ${antimode}`}></div>
-            <div className={`short-thick ${antimode}`}></div>
-            <div className={`short-thick ${antimode}`}></div>
-            <div className={`short-thin ${antimode}`}></div>
-            <div className={`short-thin ${antimode}`}></div>
-            <div className={`short-thick ${antimode}`}></div>
-          </section>
-          <div>-01-</div>
-        </div>
-        <section className="long">
-          <div className={`long-thick ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-        </section>
-        <div className="lines-and-numbers">
-          <section className="short">
-            <div className={`short-thick ${antimode}`}></div>
-            <div className={`short-thin ${antimode}`}></div>
-            <div className={`short-thin ${antimode}`}></div>
-            <div className={`short-thick ${antimode}`}></div>
-            <div className={`short-thick ${antimode}`}></div>
-            <div className={`short-thin ${antimode}`}></div>
-          </section>
-          <div>-1992-</div>
-        </div>
-        <section className="long">
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thick ${antimode}`}></div>
-          <div className={`long-thick ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-        </section>
-        <div className="lines-and-numbers">
-          <section className="short">
-            <div className={`short-thin ${antimode}`}></div>
-            <div className={`short-thin ${antimode}`}></div>
-            <div className={`short-thick ${antimode}`}></div>
-            <div className={`short-thick ${antimode}`}></div>
-            <div className={`short-thin ${antimode}`}></div>
-            <div className={`short-thick ${antimode}`}></div>
-          </section>
-          <div>----</div>
-        </div>
-        <section className="long">
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thick ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-          <div className={`long-thin ${antimode}`}></div>
-        </section>
-      </div>
-    </div>
+      </Link>
+    </>
   );
 }
 
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,19 +1,36 @@
 import React from "react";
 import { useTheme } from "../hooks/useTheme";
 import "../styles/Categories.css";
+import { Link } from "react-router-dom";
 
 function Categories({ single, categoryTitle, name, lastName }) {
   const isSingle = single === true ? "one" : "white";
   const { mode } = useTheme();
 
   return (
-    <div className={`${isSingle} ${mode}`}>
-      <h1>
-        <span>{name}</span>
-        {lastName}
-        {categoryTitle}
-      </h1>
-    </div>
+    <>
+      {categoryTitle ? (
+        <Link className="white" to={`/${categoryTitle.toLowerCase()}`}>
+          <div className={`${isSingle} ${mode}`}>
+            <h1>
+              <span>{name}</span>
+              {lastName}
+              {categoryTitle}
+            </h1>
+          </div>
+        </Link>
+      ) : (
+        <Link className="one" to="/">
+          <div className={`${isSingle} ${mode}`}>
+            <h1>
+              <span>{name}</span>
+              {lastName}
+              {categoryTitle}
+            </h1>
+          </div>
+        </Link>
+      )}
+    </>
   );
 }
 
