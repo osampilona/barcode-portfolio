c090ebe157ce03847f44b361522a390ebe45302c
osampilona <brzicamartina@gmail.com>
2021-11-25 09:43:14 +0100

    Editing text in About.js

--- src/components/About.js
+++ src/components/About.js
@@ -25,17 +25,17 @@ const About = () => {
         </div>
         <div className="text">
           <p>
-            My name is Martina Brzica and I come from Croatia. From the moment I
-            was aware about world around me I was a dreamer. Creative one. First
-            I studied History of Art at the University of Zadar, and there I
-            gained MA degree. My primary occupation in that field was
-            conservation of architectural heritage, but later I got a job as
-            assistant currator and museum pedagogist. After a while, I decided
-            to try something new and I started to work as sales representative
-            and content creator in a small IT comapny in Croatia. Later on, two
-            years ago, I have decided to move to Aalborg, Denmark to study
-            Automation Engineering at UCN. Now I live in Copenhagen, learning
-            Danish and looking for a job.
+            My name is Martina Brzica and I come from Croatia. Ever since I was
+            little, I was somewhat of a creative dreamer. I pursued my love of
+            art by studying History of Art at the University of Zadar, where I
+            gained my MA degree. My primary occupation was conservation of
+            architectural heritage, but I later got a job as an assistant
+            curator and museum pedagogist. After a while, I decided to try
+            something new and started to work as a sales representative and
+            content creator at a small IT comapny in Croatia. Two years ago I
+            decided to move to Aalborg, Denmark to study Automation Engineering
+            at UCN. Today I live in Copenhagen, learning Danish and looking for
+            a job.
           </p>
         </div>
       </section>
@@ -50,21 +50,22 @@ const About = () => {
         </div>
         <div className="text">
           <p>
-            Since we live in some challenging times, I faced lockdowns because
-            of Corona and I spent my days in hyggelig apartment in Nørresundby
-            next to the fjord. I was always interested in designing webpages, so
-            I decided to learn more about it and started few online courses. In
-            the beginning it was just designing and prototyping, but later I
-            wanted to build something by myself and started to learn HTML, CSS
-            and JavaScript. And then I wanted to learn React.js, which was great
-            thing for developing web apps.
+            Due to the circumstances at large I have mostly been staying at home
+            during the last year, and I have really been enjoying my hyggelig
+            apartment in Nørresundby next to the fjord. I was always interested
+            in web design, so I decided to learn more about it and started a few
+            online courses. In the beginning it was just designing and
+            prototyping, but I also wanted to try my hand at creating something
+            of my own. That's why I started learning HTML, CSS and JavaScript. I
+            also got proficient in React.js, which was great decidion for
+            developing web apps.
           </p>
         </div>
       </section>
       <section className="description-container">
         <div className="title-box">
           <div className="title">
-            <h3>Who hires me</h3>
+            <h3>Previous experience</h3>
           </div>
           <div className="number-box">
             <div className={`number ${antimode}`}>3</div>
@@ -72,14 +73,15 @@ const About = () => {
         </div>
         <div className="text">
           <p>
-            During my studies, I had an internship in automation company Sørig
-            Group near Aalborg. My task there was to build program for running
-            two big storages in pat food factory in France and one smaller
-            factory in USA. After graduation, I wanted to do something new and
-            put my self-learning into use, so I worked as web developer in a
-            small startup from Copenhagen, yTalent. I worked there for three
-            months as UX/UI designer and frontend developer. We made new UI for
-            company webpage and built it in code.
+            During my studies, I had an internship at Sørig Group, an automation
+            company near Aalborg. There I worked on creating a program for
+            running two large warehouses at a pat food factory in France and one
+            smaller factory in the US. After graduation, I wanted to do
+            something new and put my independent education to use, so I worked
+            as web developer at yTalent, a small startup from Copenhagen. I
+            worked there for three months as a UX/UI designer and frontend
+            developer. We created a new UI for the company web and built it in
+            code.
           </p>
         </div>
       </section>
@@ -94,17 +96,18 @@ const About = () => {
         </div>
         <div className="text">
           <p>
-            As an UX/UI designer I use all of my theoretical knowledge about art
-            and design, from my History of Art studies. For all my projects I am
-            using dotted journal, Adobe CC or Figma. Since I am amateur
-            photographer, I use a lot Photoshop and Lightroom, but for some more
-            graphic projects and ideas I use Illustrator. For designing and
-            prototyping, most of the time I use Adobe XD (because of cloud), but
-            for some projects Figma as well. After I have prepared design, I
-            start to code. At the moment, React.js with Context is my favourite
-            tool. For that I am using Visual Studio Code (VS Code), Git, GitHub
-            and Netlify. I would love to concentrate at Frontend, but with the
-            time and Backend, so I can be Fullstack one day.
+            As an UX/UI designer I use my vast theoretical knowledge about art
+            and design from my History of Art studies. I normally work with
+            dotted journal, Adobe CC or Figma. As an amateur photographer, I use
+            Photoshop and Lightroom a lot, but I also know how to work in
+            Illustrator for some more graphic projects and ideas. When designing
+            and prototyping I mostly work in Adobe XD, but I am also comfortable
+            working in Figma. After preparing the project design, I start to
+            code. At the moment, React.js with Context is my favourite tool. I
+            use Visual Studio Code (VS Code), Git, GitHub and Netlify. At the
+            moment I would love to concentrate on Frontend, but with time I plan
+            on working on my Backend skills as well, so I can be Fullstack one
+            day.
           </p>
         </div>
       </section>
@@ -119,14 +122,13 @@ const About = () => {
         </div>
         <div className="text">
           <p>
-            Taking recommendations from all of my preivous job, managers,
-            colleagues and friends, first of all, they would say that I am loyal
-            employee, colleague and a friend. Second thing would be that I am
-            persistant, goal oriented and detailed. And finaly, that I am
-            open-minded. I deeply believe that listening and dialog are one of
-            the best things that can happen between colleagues which leads to
-            common understanding, especially in small but fast growing
-            companies.
+            Based on the feedback from my previous work, managers, colleagues
+            and friends, I think I can say I am a loyal employee, colleague and
+            a friend. I am also persistant, goal-oriented and detailed. Finaly,
+            I am open-minded. I deeply believe that listening to your colleagues
+            and engaging in a constructive dialogue with them is a key for a
+            good workplace, as it leads to common understanding, especially in
+            small but fast growing companies.
           </p>
         </div>
       </section>
@@ -141,13 +143,12 @@ const About = () => {
         </div>
         <div className="text">
           <p>
-            You can expect creative observer with flare for organization and
-            solving problems. As from UX/UI designer, you could find me as a
-            detailed researcher who brainstorm and discuss ideas. I would say
-            that my moto would be "Less is more". As from a frontend developer,
-            you could find in me a person who want to get a chance to learn and
-            practice more. Someone who is looking for a good mentor and strive
-            for more.
+            You can expect a creative observer with a flare for organization and
+            problem solving. As a UX/UI designer, I am a detailed researcher who
+            loves to brainstorm and discuss ideas. I would say that my moto is
+            "Less is more". As a frontend developer, I would love to get a
+            chance to learn and practice more. I am someone who is looking for a
+            good mentor and strive for development.
           </p>
         </div>
       </section>
